7fbcc58fa76ae8d186a3e59fb06ec0ea035ec40c
hayat <hayatabdulkerim11@gmail.com>
2026-09-21 15:12:23 +0300

    async with out auth

--- database.py
+++ database.py
@@ -1,25 +1,25 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./blog.db"
 
-
-engine = create_engine(
-  SQLALCHEMY_DATABASE_URL,
-  connect_args={"check_same_thread": False}, # sqlite specific 
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},# sqlite specific 
 )
-
-# Factory that creates a session for each and every database interaction
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+ 
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
 # this class is used to tell sqlalchemy that the classes that will inherit from it will be database tables
 class Base(DeclarativeBase):
-  pass
+    pass
 
 # Create a database session, call it db, and automatically clean it up when we're finished
 # It is a dependency function which is simply a normal Python function that provides something an endpoint needs.
-def get_db():
-  with SessionLocal() as db:
-    yield db
-
-    
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session   
