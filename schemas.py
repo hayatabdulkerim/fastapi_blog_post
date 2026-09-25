1f40b163444f07f96d66d187aecad175d8e66aea
hayat <hayatabdulkerim11@gmail.com>
2026-09-25 11:30:59 +0300

    added file upload functionality

--- schemas.py
+++ schemas.py
@@ -9,6 +9,7 @@ from pydantic import (
 
 # user schemas _____________________________________________________________________________
 
+
 class UserBase(BaseModel):
     username: str = Field(min_length=1, max_length=50)
     email: EmailStr = Field(max_length=120)
@@ -21,7 +22,7 @@ class UserCreate(UserBase):
 class UserPublic(BaseModel):
     # this model is used in a response model which recives a user object from the db  but pydantic expencts a dictionary so this model config tells pydantic that this is an object and it can look the attributes to get the values needed for validation
 
-    model_config = ConfigDict(from_attributes=True) 
+    model_config = ConfigDict(from_attributes=True)
 
     id: int
     username: str
@@ -36,15 +37,14 @@ class UserPrivate(UserPublic):
 class UserUpdate(BaseModel):
     username: str | None = Field(default=None, min_length=1, max_length=50)
     email: EmailStr | None = Field(default=None, max_length=120)
-    image_file: str | None = Field(default=None, min_length=1, max_length=200)
 
 
 # token schema _______________________________________________________________________________
 
+
 class Token(BaseModel):
     access_token: str
     token_type: str
-
 
 
 # post schema _______________________________________________________________________________
@@ -61,13 +61,11 @@ class PostCreate(PostBase):  # request model
 
 class PostUpdate(BaseModel):
     title: str | None = Field(default=None, min_length=1, max_length=100)
-    content: str | None  = Field(default=None, min_length=1)
+    content: str | None = Field(default=None, min_length=1)
 
 
 class PostResponse(PostBase):  # response model
-    model_config = ConfigDict(
-        from_attributes=True
-    )
+    model_config = ConfigDict(from_attributes=True)
 
     id: int
     user_id: int
